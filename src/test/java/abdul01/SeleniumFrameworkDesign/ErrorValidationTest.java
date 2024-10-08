6706adf8968e742de4e69e22b9801633b9d23386
AbdulRazak151 <abdulrazakt11@gmail.com>
2024-10-08 15:42:38 +0530

    indian

--- src/test/java/abdul01/SeleniumFrameworkDesign/ErrorValidationTest.java
+++ src/test/java/abdul01/SeleniumFrameworkDesign/ErrorValidationTest.java
@@ -44,6 +44,14 @@ public class ErrorValidationTest extends BaseTest {
 	    System.out.println("4");
 	    System.out.println("5");
 	    
+	    
+	    
+	    System.out.println("1");
+	    System.out.println("2");
+	    System.out.println("3");
+	    System.out.println("4");
+	    System.out.println("5");
+	    
 }
 	/*@Test
 	public void ProductErrorValidation() throws Exception {
